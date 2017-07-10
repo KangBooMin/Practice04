22cbeac41e92d39d893f6772b7bb9e4844c6621b
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:40:56 +0900

    1st

--- src/com/javaex/problem01/MyBase.java
+++ src/com/javaex/problem01/MyBase.java
@@ -2,6 +2,16 @@ package com.javaex.problem01;
 
 public class MyBase extends Base{
 
-    //코드작성
-    
+    public void service(String state) {
+
+        if (state.equals("낮")) {
+            System.out.println("낮에는 열심히 일하자");
+        } else if (state.equals("밤")) {
+        	night();
+        } else if (state.equals("오후")) {
+        	System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+        }
+        
+    }
+   
 }
