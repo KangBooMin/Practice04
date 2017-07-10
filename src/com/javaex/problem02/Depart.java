c2f2c9ba9e75568139d48f1cad99bc3a7bf0288c
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-10 17:57:26 +0900

    1st

--- src/com/javaex/problem02/Depart.java
+++ src/com/javaex/problem02/Depart.java
@@ -2,6 +2,27 @@ package com.javaex.problem02;
 
 public class Depart extends Employee {
 
-    //코드작성
+    private String department;
+    
+    public Depart(String name, int salary, String department) {
+    	super(name, salary);
+    	this.setDepartment(department);
+    }
+
+    public Depart(String department) {
+    	this.department = department;
+    }
+    
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+    
+	public void getInformation() {
+        System.out.println("이름:" + super.getName() + "  연봉:" + super.getSalary() + " 부서:"+department);
+    }
 
 }
