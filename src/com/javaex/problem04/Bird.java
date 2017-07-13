2a379324f9a640b6c83b90c92633f3364e5245be
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-13 11:13:21 +0900

    4

--- src/com/javaex/problem04/Bird.java
+++ src/com/javaex/problem04/Bird.java
@@ -2,7 +2,19 @@ package com.javaex.problem04;
 
 public abstract class Bird {
     private String name;
+    
+    
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	
+	public void sing() {}
 
-
+    public void fly() {}
+    
+    public void showName() {}
 }
